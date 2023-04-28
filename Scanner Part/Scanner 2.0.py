070ab57c2b88c70bcb52e78249b1c796673c96e1
Programerdarshu <113490406+Programerdarshu@users.noreply.github.com>
2023-04-28 09:06:07 +0530

    Update Scanner 2.0.py
    Port Scanner code which scans 1000 ports

--- Scanner Part/Scanner 2.0.py
+++ Scanner Part/Scanner 2.0.py
@@ -1,1 +1,19 @@
+#!/user/bin/python
 
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(2)
+
+host = input("[*] Enter The Host To Scan:  ")
+
+def portscanner(port):
+    if sock.connect_ex((host,port)):
+            print("[-] Port %d is colsed" % (port))
+    else:
+            print("[-] Port %d is colsed" % (port))
+
+for port in range(1,1000):
+       portscanner(port)
+    
+    #you can edit number port to scan 
